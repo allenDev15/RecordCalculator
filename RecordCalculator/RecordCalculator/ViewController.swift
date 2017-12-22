7681b71333b1f7022a44f13a63b38e0894f34cc9
allen <licong@EfunddeiMac.local>
2017-12-22 11:20:27 -0800

    UI 初步搭建

--- RecordCalculator/RecordCalculator/ViewController.swift
+++ RecordCalculator/RecordCalculator/ViewController.swift
@@ -11,43 +11,124 @@ import UIKit
 
 
 class ViewController: UIViewController {
-    enum EnumBtnType:Int {
-        case enumBtnTypeC = 100
-        case enumBtnTypeR = 101
-        case enumBtnTypeAOrS = 102
-        case enumBtnTypeDiv = 103
-        case enumBtnType7 = 104
-        case enumBtnType8 = 105
-        case enumBtnType9 = 106
-        case enumBtnTypeMul = 107
-        case enumBtnType4 = 108
-        case enumBtnType5 = 109
-        case enumBtnType6 = 110
-        case enumBtnTypeSub = 111
-        case enumBtnType1 = 112
-        case enumBtnType2 = 113
-        case enumBtnType3 = 114
-        case enumBtnTypeAdd = 115
-        case enumBtnType0 = 116
-        case enumBtnTypePoint = 117
-        case enumBtnTypePercent = 118
-        case enumBtnTypeEqu = 119
-    }
-    @IBAction func onBtnClick(_ sender: UIButton) {
-        let tag = sender.tag
-        print(tag)
-    }
+   
+    @IBOutlet weak var lbInput: UILabel!
+    var isFinishOperate = false
     
+    //MARK: - 视图生命周期
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: - 用户操作
+    @IBAction func onNumberClick(_ sender: UIButton) {
+        
+        let tag = sender.tag
+        let newInput_str = self.changeNumberInputWithTap(tag: tag)
+        var newOutPut_str : String
+        if isFinishOperate==true {
+            newOutPut_str = self.inputingNumber(orginString: "0", inputString: newInput_str)
+        }else {
+            newOutPut_str = self.inputingNumber(orginString: lbInput.text!, inputString: newInput_str)
+        }
+        lbInput.text = newOutPut_str
+        print(newInput_str)
+    }
+    
+    func inputingNumber(orginString:String,inputString:String) -> String {
+        var newOutString:String
+        if orginString == "0" {
+            newOutString = inputString
+        }else {
+            newOutString = orginString.appending(inputString)
+        }
+        if newOutString == "." {
+            newOutString = "0."
+        }
+        return newOutString
+    }
+    
+    @IBAction func onBasicsOperatorClick(_ sender: UIButton) {
+        
+        let tag = sender.tag
+        print(tag)
+    }
+    @IBAction func onSeniorOperatorClick(_ sender: UIButton) {
+        let tag = sender.tag
+        print(tag)
+    }
+    
+    //MARK: - 工具
+    func changeNumberInputWithTap(tag:Int) -> String {
+        //100-110 0~.
+        var tap_str : String
+        switch tag {
+        case 100:
+            tap_str = "0"
+        case 101:
+            tap_str = "1"
+        case 102:
+            tap_str = "2"
+        case 103:
+            tap_str = "3"
+        case 104:
+            tap_str = "4"
+        case 105:
+            tap_str = "5"
+        case 106:
+            tap_str = "6"
+        case 107:
+            tap_str = "7"
+        case 108:
+            tap_str = "8"
+        case 109:
+            tap_str = "9"
+        case 110:
+            tap_str = "."
+        default:
+            tap_str = ""
+        }
+        return tap_str
+    }
 
+    func changeBasicsOperatorInputWithTap(tag:Int) -> String {
+        //201-209
+        var tap_str : String
+        switch tag {
+        case 100:
+            tap_str = "0"
+        case 101:
+            tap_str = "1"
+        case 102:
+            tap_str = "2"
+        case 103:
+            tap_str = "3"
+        case 104:
+            tap_str = "4"
+        case 105:
+            tap_str = "5"
+        case 106:
+            tap_str = "6"
+        case 107:
+            tap_str = "7"
+        case 108:
+            tap_str = "8"
+        case 109:
+            tap_str = "9"
+        case 110:
+            tap_str = "."
+        default:
+            tap_str = ""
+        }
+        return tap_str
+    }
 
 }
 
